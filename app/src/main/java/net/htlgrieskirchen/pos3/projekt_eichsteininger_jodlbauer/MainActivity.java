cd76cb663971e93b7fb58e143ab608524f9e1deb
Jakob7203 <joja2003@live.at>
2020-06-03 13:53:28 +0200

    change Buttons to ImageButtons

--- app/src/main/java/net/htlgrieskirchen/pos3/projekt_eichsteininger_jodlbauer/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/pos3/projekt_eichsteininger_jodlbauer/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,18 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnYouTube = findViewById(R.id.btnYouTube);
+        ImageButton btnYouTube = findViewById(R.id.btnYouTube);
         btnYouTube.setOnClickListener((View v) -> {
             Intent intent = new Intent(this, YoutubeConvertMenu.class);
             startActivity(intent);
         });
-        Button btnCamera = findViewById(R.id.btnCamera);
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, CameraConvertMenu.class);
-                startActivity(i);
-            }
+        ImageButton btnCamera = findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, CameraConvertMenu.class);
+            startActivity(i);
         });
 
     }
